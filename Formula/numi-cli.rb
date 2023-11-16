544382dcdf84ddd48ea750aa625f577e795274d2
Release workflow <release@numi.app>
2023-11-16 00:57:18 +0000

    numi-cli.rb v0.8

--- Formula/numi-cli.rb
+++ Formula/numi-cli.rb
@@ -3,19 +3,19 @@ class NumiCli < Formula
   homepage "https://numi.app"
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://v4.numi.app/v4/v0.7/numi-cli-v0.7-x86_64-apple-darwin.tar.gz"
-      sha256 "41819d8776cf0c5e610df7fa32ef49b3e8c2bce63b447f9b1f0538adc101ae31"
+      url "https://v4.numi.app/v4/v0.8/numi-cli-v0.8-x86_64-apple-darwin.tar.gz"
+      sha256 "06591244ad22772c472342eda2a9caf7a30b865260f8d055293c68bb5fa55edf"
     elsif Hardware::CPU.arm?
-      url "https://v4.numi.app/v4/v0.7/numi-cli-v0.7-aarch64-apple-darwin.tar.gz"
-      sha256 "a1d17049c3a53c38a7bb2c67018556d1185c9f7738cc9bac905d4e9be6901438"
+      url "https://v4.numi.app/v4/v0.8/numi-cli-v0.8-aarch64-apple-darwin.tar.gz"
+      sha256 "8b63b98a7ddc8c811065a47493eaa0061af4032776cb05a3639d787c4a24b033"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://v4.numi.app/v4/v0.7/numi-cli-v0.7-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "459d17bb4ddb45687f5a1ac0d4b5db436a04fb0cc886e65bd4c644c6670bbc85"
+      url "https://v4.numi.app/v4/v0.8/numi-cli-v0.8-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "788ac08b82711353389b697b444aa07ede4887b5b0ec1c94c0c8798e8cacaea0"
     elsif Hardware::CPU.arm?
-      url "https://v4.numi.app/v4/v0.7/numi-cli-v0.7-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "97fda075b1f7059a8be9114fff06f95886b35c6636f283ccc15def4da78c1e88"
+      url "https://v4.numi.app/v4/v0.8/numi-cli-v0.8-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "a785d0569e168712282f1eeee923925c3148afdabc627c5de5315c05639e91b6"
     end
   end
   def install
