2515859719635426fd6e017f79f308062df9d97d
Daniel Bayley <daniel.bayley@me.com>
2024-05-05 12:47:48 +0100

    Fix broken `404` Amazon S3 download links

--- Formula/numi-cli.rb
+++ Formula/numi-cli.rb
@@ -3,18 +3,18 @@ class NumiCli < Formula
   homepage "https://numi.app"
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://s3.numi.app/v4/v0.11.0/numi-cli-v0.11.0-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/nikolaeu/numi/releases/download/cli-v0.11.0/numi-cli-v0.11.0-x86_64-apple-darwin.tar.gz"
       sha256 "3276676ddc934bcac19457a26e923d44635063b08a5883eae7322e0d2b9308c6"
     elsif Hardware::CPU.arm?
-      url "https://s3.numi.app/v4/v0.11.0/numi-cli-v0.11.0-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/nikolaeu/numi/releases/download/cli-v0.11.0/numi-cli-v0.11.0-aarch64-apple-darwin.tar.gz"
       sha256 "7b8fa5c1dfd028e453cd442c02097c903b5bcae60abd3a173401f96c9c2e480d"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://s3.numi.app/v4/v0.11.0/numi-cli-v0.11.0-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/nikolaeu/numi/releases/download/cli-v0.11.0/numi-cli-v0.11.0-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "9708ea859cee4c8cc5a25e3fb6fd388022f223ddca6211f1d5ccf972fbc160d5"
     elsif Hardware::CPU.arm?
-      url "https://s3.numi.app/v4/v0.11.0/numi-cli-v0.11.0-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/nikolaeu/numi/releases/download/cli-v0.11.0/numi-cli-v0.11.0-aarch64-unknown-linux-gnu.tar.gz"
       sha256 "34df0c1d9ab7abd3b0b8e06ca659a1e8075b0567125738d948793ec6882844ca"
     end
   end
